5469ff52c9ff991925f9040051b768ac42bdc06a
OxOcs1090 <cs16forever0x0@proton.me>
2023-07-19 08:49:41 +0800

    Update: docusaurus.config.js: favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Constrix Docs',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'https://avatars.githubusercontent.com/u/138226524?s=200&v=4',
 
   // Set the production url of your site here
   url: 'https://your-docusaurus-test-site.com',
