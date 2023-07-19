53864b36cb805f013c8947e1ee15ce89cdcae0b5
OxOcs1090 <cs16forever0x0@proton.me>
2023-07-19 08:49:22 +0800

    Update: docusaurus.config.js: title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,18 +8,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Constrix Docs',
   tagline: 'Dinosaurs are cool',
-  favicon: 'https://avatars.githubusercontent.com/u/138226524?s=200&v=4',
+  favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.constrix.org',
+  url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Constrix', // Usually your GitHub org/user name.
-  projectName: 'Constrix-docs', // Usually your repo name.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
